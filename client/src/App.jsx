73955db5c98c4a2efa37e7dad55574c51be7071f
biswasuser <user.lohanibiswas1@gmail.com>
2025-05-24 14:17:41 +0545

    made seller section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,16 +12,21 @@ import ProductDetails from './pages/ProductDetails'
 import Cart from './pages/Cart'
 import AddAddress from './pages/AddAddress'
 import Myorders from './pages/Myorders'
+import SellerLogin from './components/seller/SellerLogin'
+import SellerLayout from './pages/Seller/SellerLayout'
+import AddProduct from './pages/Seller/AddProduct'
+import ProductList from './pages/Seller/ProductList'
+import Orders from './pages/Seller/Orders'
 
 
 
 const App = () => {
 
     const isSellerPath = useLocation().pathname.includes("seller")
-    const {showUserLogin} = useAppContext()
+    const {showUserLogin, isSeller} = useAppContext()
 
   return (
-    <div>
+    <div className='text-default  min-h-screen text-gray-700 bg-white'>
       {
         isSellerPath ? null :  <Navbar/>
       
@@ -40,11 +45,20 @@ const App = () => {
          <Route path='/cart' element={<Cart/>}/>
          <Route path='/add-address' element={<AddAddress/>}/>
          <Route path='/myorders' element={<Myorders/>}/>
+         <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin/>}> 
+           <Route index element={isSeller ? <AddProduct/>: null}/>
+           <Route path='product-list' element={<ProductList/>}/>
+           <Route path='orders' element={<Orders/>}/>
+         </Route>
+
 
         
         </Routes>
       </div>
-      <BottomBanner/>
+      {
+
+      isSellerPath ? null : <BottomBanner/>
+      }
     
     </div>
   )
