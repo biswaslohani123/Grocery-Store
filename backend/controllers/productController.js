81d3fe7d245e59c1536d7c388d22e89989a74dc2
biswasuser <user.lohanibiswas1@gmail.com>
2025-05-27 08:03:12 +0545

    made productModel

---
+++ backend/controllers/productController.js
@@ -0,0 +1,22 @@
+
+
+// add product
+const addProduct = async (req, res) => {
+
+}
+
+// product List
+const ProductList = async (req, res) => {
+
+}
+// productById get single product
+const ProductById = async (req, res) => {
+
+}
+// Change Stock
+const ChangeStock= async (req, res) => {
+
+}
+
+
+export {addProduct}
